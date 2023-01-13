188f2017ba7223385822714f72774e586f3d542a
yonas-topgrep <yonas71298@gmail.com>
2023-01-13 09:57:25 +0530

    Added addition

---
+++ src/demo/Addition.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a demo for git E2E showing addition");
+	}
+
+}

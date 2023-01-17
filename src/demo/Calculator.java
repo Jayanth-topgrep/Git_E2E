508ec50a933913fc9511e434cd0cb4544d4e280d
Jayanth-topgrep <jayanth.d@topgrep.com>
2023-01-17 08:20:36 +0530

    The bug is fixed here

--- src/demo/Calculator.java
+++ src/demo/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a demo for git E2E");
+		System.out.println("This is to add the bugfix there");
 	}
 
 }

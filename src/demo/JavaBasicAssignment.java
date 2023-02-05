334ba0481353cc74c19963b76baaf15bd42d3f80
Jayanth-topgrep <jayanth.d@topgrep.com>
2023-02-05 22:24:47 +0530

    changes for scanner object

--- src/demo/JavaBasicAssignment.java
+++ src/demo/JavaBasicAssignment.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class JavaBasicAssignment {
 
 	public static void main(String[] args) {
-		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		Scanner sc = new Scanner(System.in);  // Create a Scanner object
 	    System.out.println("Enter value of X");
-	    int X = myObj.nextInt();
+	    int X = sc.nextInt();
 		System.out.println("Enter value of Y");
-		int Y = myObj.nextInt();
+		int Y = sc.nextInt();
 
 		System.out.println(String.format("Addition value is %s", X+Y));
 
